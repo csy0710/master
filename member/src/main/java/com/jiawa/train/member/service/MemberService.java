9248dccbb6f8c2d99f74aef448815ac8c6517ecf
csy0710 <csy0710@qq.com>
2025-08-29 21:29:00 +0800

    封装请求参数和返回结果

--- member/src/main/java/com/jiawa/train/member/service/MemberService.java
+++ member/src/main/java/com/jiawa/train/member/service/MemberService.java
@@ -4,6 +4,7 @@ import cn.hutool.core.collection.CollUtil;
 import com.jiawa.train.member.domain.Member;
 import com.jiawa.train.member.domain.MemberExample;
 import com.jiawa.train.member.mapper.MemberMapper;
+import com.jiawa.train.member.req.MemberRegisterReq;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
 
@@ -18,7 +19,8 @@ public class MemberService {
     }
 
 
-    public long register(String mobile) {
+    public long register(MemberRegisterReq req) {
+        String mobile=req.getMobile();
         MemberExample memberExample = new MemberExample();//创建一个Example
         memberExample.createCriteria().andMobileEqualTo(mobile);//将相同值的mobile赋给example
         List<Member> list = memberMapper.selectByExample(memberExample);//将查找到的相同的mobile存入list中
