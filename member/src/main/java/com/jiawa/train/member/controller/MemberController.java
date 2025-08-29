0f2cc5b9d49fc911882015cfe508085a462d7b46
csy0710 <csy0710@qq.com>
2025-08-29 20:01:57 +0800

    实现一个简单的注册接口

--- member/src/main/java/com/jiawa/train/member/controller/MemberController.java
+++ member/src/main/java/com/jiawa/train/member/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.jiawa.train.member.controller;
 import com.jiawa.train.member.service.MemberService;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,4 +18,9 @@ public class MemberController {
         return memberService.count();
     }
 
+    @PostMapping("/register")
+    public long register(String mobile){
+
+        return memberService.register(mobile);
+    }
 }
--- member/src/main/java/com/jiawa/train/member/service/MemberService.java
+++ member/src/main/java/com/jiawa/train/member/service/MemberService.java
@@ -1,8 +1,13 @@
 package com.jiawa.train.member.service;
 
+import cn.hutool.core.collection.CollUtil;
+import com.jiawa.train.member.domain.Member;
+import com.jiawa.train.member.domain.MemberExample;
 import com.jiawa.train.member.mapper.MemberMapper;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MemberService {
@@ -10,5 +15,24 @@ public class MemberService {
     private MemberMapper memberMapper;
     public int count(){
         return Math.toIntExact(memberMapper.countByExample(null));
+    }
+
+
+    public long register(String mobile) {
+        MemberExample memberExample = new MemberExample();//创建一个Example
+        memberExample.createCriteria().andMobileEqualTo(mobile);//将相同值的mobile赋给example
+        List<Member> list = memberMapper.selectByExample(memberExample);//将查找到的相同的mobile存入list中
+        if (CollUtil.isNotEmpty(list)){
+//            return list.get(0).getId();
+            throw new RuntimeException("手机号已注册");
+
+        }
+
+        Member member=new Member();
+        member.setId(System.currentTimeMillis());
+        member.setMobile(mobile);
+
+        memberMapper.insert(member);
+        return member.getId();
     }
 }
