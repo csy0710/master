d6ef9d1a25edca18c4da82b86aacd310066261db
csy0710 <csy0710@qq.com>
2025-09-05 20:47:07 +0800

    为axios增加登录相关拦截：请求时带上token，返回时校验码是不是401

--- web/src/views/main.vue
+++ web/src/views/main.vue
@@ -6,15 +6,19 @@
       <a-layout-content
           :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
       >
+        所有会员总数：{{count}}
         <router-view></router-view>
       </a-layout-content>
     </a-layout>
   </a-layout>
 </template>
 <script>
-import { defineComponent } from 'vue';
+import {defineComponent, ref} from 'vue';
 import TheHeaderView from "@/components/the-header";
 import TheSiderView from "@/components/the-sider";
+import axios from "axios";
+import {notification} from "ant-design-vue";
+// import store from "@/store";
 
 export default defineComponent({
   components: {
@@ -22,7 +26,18 @@ export default defineComponent({
     TheHeaderView,
   },
   setup() {
+    const count=ref(0);//ref声明基本的数据类型
+    axios.get("/member/member/count").//直接传递实体
+        then((response)=> {
+          let data = response.data;//获取结果的数据
+          if (data.success) {
+            count.value = data.content;
+          } else {
+            notification.error({description: data.message});
+          }
+        })
     return {
+      count
     };
   },
 });
