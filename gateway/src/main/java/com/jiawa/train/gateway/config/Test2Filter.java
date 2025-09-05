f441b197800118fba41e06fbc6b06dd031550068
csy0710 <csy0710@qq.com>
2025-09-05 18:41:48 +0800

    演示gateway过滤器的使用

---
+++ gateway/src/main/java/com/jiawa/train/gateway/config/Test2Filter.java
@@ -0,0 +1,27 @@
+package com.jiawa.train.gateway.config;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+//实现tokne校验拦截器
+public class Test2Filter implements GlobalFilter , Ordered {//Ordered用于决定过滤器使用顺序
+    private static final Logger LOG = LoggerFactory.getLogger(Test2Filter.class);
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        LOG.info("Test2Filter");
+        return chain.filter(exchange);//这个过滤器走完走下一个过滤器
+    }
+
+    @Override
+    public int getOrder() {//Ordered用于决定过滤器使用顺序
+        return 0;
+    }
+}
