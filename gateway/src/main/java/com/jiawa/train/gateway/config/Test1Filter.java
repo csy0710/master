f37f39c060d682c2c7548a0232b4ad90d7e682a2
csy0710 <csy0710@qq.com>
2025-09-05 19:09:29 +0800

    为gateway增加登录校验过滤器

--- gateway/src/main/java/com/jiawa/train/gateway/config/Test1Filter.java
+++ gateway/src/main/java/com/jiawa/train/gateway/config/Test1Filter.java
@@ -1,27 +1,27 @@
-package com.jiawa.train.gateway.config;
-
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.cloud.gateway.filter.GatewayFilterChain;
-import org.springframework.cloud.gateway.filter.GlobalFilter;
-import org.springframework.core.Ordered;
-import org.springframework.stereotype.Component;
-import org.springframework.web.server.ServerWebExchange;
-import reactor.core.publisher.Mono;
-
-@Component
-//实现tokne校验拦截器
-public class Test1Filter implements GlobalFilter , Ordered {
-    private static final Logger LOG = LoggerFactory.getLogger(Test1Filter.class);
-    @Override
-    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        LOG.info("Test1Filter");
-        return chain.filter(exchange);//这个过滤器走完走下一个过滤器
-    }
-
-    @Override
-    public int getOrder() {
-        return 1;
-    }
-}
+//package com.jiawa.train.gateway.config;
+//
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+//import org.springframework.cloud.gateway.filter.GlobalFilter;
+//import org.springframework.core.Ordered;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.server.ServerWebExchange;
+//import reactor.core.publisher.Mono;
+//
+//@Component
+////实现tokne校验拦截器
+//public class Test1Filter implements GlobalFilter , Ordered {
+//    private static final Logger LOG = LoggerFactory.getLogger(Test1Filter.class);
+//    @Override
+//    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+//        LOG.info("Test1Filter");
+//        return chain.filter(exchange);//这个过滤器走完走下一个过滤器
+//    }
+//
+//    @Override
+//    public int getOrder() {
+//        return 0;
+//    }
+//}
