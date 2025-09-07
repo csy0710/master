1cc85fb5a3d824beeb678474ad494033eeec698d
csy0710 <csy0710@qq.com>
2025-09-07 20:19:07 +0800

    分装分页返回结果 格式化返回日期的字段

--- member/src/main/java/com/jiawa/train/member/service/PassengerService.java
+++ member/src/main/java/com/jiawa/train/member/service/PassengerService.java
@@ -4,7 +4,9 @@ import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.date.DateTime;
 import cn.hutool.core.util.ObjectUtil;
 import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.jiawa.train.common.context.LoginMemberContext;
+import com.jiawa.train.common.resp.PageResp;
 import com.jiawa.train.common.util.SnowUtil;
 import com.jiawa.train.member.domain.Passenger;
 import com.jiawa.train.member.domain.PassengerExample;
@@ -13,12 +15,15 @@ import com.jiawa.train.member.req.PassengerQueryReq;
 import com.jiawa.train.member.req.PassengerSaveReq;
 import com.jiawa.train.member.resp.PassengerQueryResp;
 import jakarta.annotation.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class PassengerService {
+      public static final Logger LOG = LoggerFactory.getLogger(PassengerService.class);
     @Resource
     private PassengerMapper passengerMapper;
     public void save(PassengerSaveReq req){
@@ -32,17 +37,31 @@ public class PassengerService {
         passengerMapper.insert(passenger);
     }
 
-    public List<PassengerQueryResp> queryList(PassengerQueryReq req){
+    public PageResp<PassengerQueryResp> queryList(PassengerQueryReq req){
         PassengerExample passengerExample = new PassengerExample();// 创建MyBatis的Example查询对象
         PassengerExample.Criteria criteria = passengerExample.createCriteria();    // 创建查询条件Criteria对象
         if (ObjectUtil.isNotNull(req.getMemberId())){// 条件判断：如果请求参数中的会员ID不为空，则添加会员ID等于条件
             criteria.andMemberIdEqualTo(req.getMemberId());
         } // 执行查询，获取乘客实体列表
+        /*在PassengerQueryReq req传入的参数中包含页码和页数*/
+        LOG.info("查询页码：{}",req.getPage());
+        LOG.info("每条页数：{}",req.getSize());
+        /*分页组件*/
         PageHelper.startPage(req.getPage(), req.getSize());
+        /*执行分页查询，得到的是“当前页”的数据列表，List大小 <= req.getSize()*/
         List<Passenger> passengerList = passengerMapper.selectByExample(passengerExample);
         // 将实体列表转换为响应对象列表（DTO转换）
         List<PassengerQueryResp> List = BeanUtil.copyToList(passengerList, PassengerQueryResp.class);
-        return List;
+        /*获取包含“总记录数”和“当前页数据”的分页信息对象*/
+        PageInfo<Passenger> pageInfo = new PageInfo<>(passengerList);
+        LOG.info("总行数：{}",pageInfo.getTotal());
+        LOG.info("总页数：{}",pageInfo.getPages());
+       /* 创建自定义的响应对象 PageResp*/
+        PageResp<PassengerQueryResp> pageResp = new PageResp<>();
+        /*将“总记录数”和“当前页的DTO列表”设置到自定义响应对象中*/
+        pageResp.setTotal(pageInfo.getTotal());
+        pageResp.setList(List);
+        return pageResp;
     }
 
 
