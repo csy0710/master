3f565a09f7d09ffea9ab11238b35ffe1bbe82397
csy0710 <csy0710@qq.com>
2025-09-08 19:18:26 +0800

    写一个简单的代码生成器

---
+++ generator/src/main/java/com/jiawa/train/generator/server/ServerGenerator.java
@@ -0,0 +1,23 @@
+package com.jiawa.train.generator.server;
+
+import com.jiawa.train.generator.util.FreemarkerUtil;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ServerGenerator {
+    static String toPath = "generator/src/main/java/com/jiawa/train/generator/test/";
+    static {
+      new  File(toPath).mkdirs();
+    }
+    public static void main(String[] args) throws Exception {
+
+        FreemarkerUtil.initConfig("test.ftl");
+        Map<String, Object> param = new HashMap<>();
+        param.put("domain","Test");
+        FreemarkerUtil.generator(toPath + "Test.java",param);
+
+    }
+
+}
