df590609c15cf46a191eeff4d86580d8cddb1fc0
csy0710 <csy0710@qq.com>
2025-09-08 20:29:48 +0800

    使用passengerService制作模板，生成service成功

---
+++ generator/src/main/java/com/jiawa/train/generator/ftl/service.ftl
@@ -0,0 +1,80 @@
+package com.jiawa.train.member.service;
+
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.date.DateTime;
+import cn.hutool.core.util.ObjectUtil;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.jiawa.train.common.context.LoginMemberContext;
+import com.jiawa.train.common.resp.PageResp;
+import com.jiawa.train.common.util.SnowUtil;
+import com.jiawa.train.member.domain.${Domain};
+import com.jiawa.train.member.domain.${Domain}Example;
+import com.jiawa.train.member.mapper.${Domain}Mapper;
+import com.jiawa.train.member.req.${Domain}QueryReq;
+import com.jiawa.train.member.req.${Domain}SaveReq;
+import com.jiawa.train.member.resp.${Domain}QueryResp;
+import jakarta.annotation.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ${Domain}Service {
+      public static final Logger LOG = LoggerFactory.getLogger(${Domain}Service.class);
+    @Resource
+    private ${Domain}Mapper ${domain}Mapper;
+    public void save(${Domain}SaveReq req){
+        DateTime now = DateTime.now();
+        // 将请求对象req的属性复制到${Domain}对象中（需要确保两个类的属性名和类型匹配）
+        ${Domain} ${domain} = BeanUtil.copyProperties(req, ${Domain}.class);
+        if (ObjectUtil.isNull(${domain}.getId())){/*根据id判断是新增保存还是编辑保存*/
+            /*新增保存*/
+            ${domain}.setMemberId(LoginMemberContext.getId());//拦截器获取headers数据，得到用户id
+            ${domain}.setId(SnowUtil.getSnowflakeNextId());
+            ${domain}.setCreateTime(now);
+            ${domain}.setUpdateTime(now);
+            ${domain}Mapper.insert(${domain});
+        }else {/*编辑保存*/
+            ${domain}.setUpdateTime(now);
+            ${domain}Mapper.updateByPrimaryKey(${domain});
+        }
+
+    }
+
+    public PageResp<${Domain}QueryResp> queryList(${Domain}QueryReq req){
+        ${Domain}Example ${domain}Example = new ${Domain}Example();// 创建MyBatis的Example查询对象
+        ${domain}Example.setOrderByClause("id desc");
+        ${Domain}Example.Criteria criteria = ${domain}Example.createCriteria();    // 创建查询条件Criteria对象
+        if (ObjectUtil.isNotNull(req.getMemberId())){// 条件判断：如果请求参数中的会员ID不为空，则添加会员ID等于条件
+            criteria.andMemberIdEqualTo(req.getMemberId());
+        } // 执行查询，获取乘客实体列表
+        /*在${Domain}QueryReq req传入的参数中包含页码和页数*/
+        LOG.info("查询页码：{}",req.getPage());
+        LOG.info("每条页数：{}",req.getSize());
+        /*分页组件*/
+        PageHelper.startPage(req.getPage(), req.getSize());
+        /*执行分页查询，得到的是“当前页”的数据列表，List大小 <= req.getSize()*/
+        List<${Domain}> ${domain}List = ${domain}Mapper.selectByExample(${domain}Example);
+        // 将实体列表转换为响应对象列表（DTO转换）
+        List<${Domain}QueryResp> List = BeanUtil.copyToList(${domain}List, ${Domain}QueryResp.class);
+        /*获取包含“总记录数”和“当前页数据”的分页信息对象*/
+        PageInfo<${Domain}> pageInfo = new PageInfo<>(${domain}List);
+        LOG.info("总行数：{}",pageInfo.getTotal());
+        LOG.info("总页数：{}",pageInfo.getPages());
+       /* 创建自定义的响应对象 PageResp*/
+        PageResp<${Domain}QueryResp> pageResp = new PageResp<>();
+        /*将“总记录数”和“当前页的DTO列表”设置到自定义响应对象中*/
+        pageResp.setTotal(pageInfo.getTotal());
+        pageResp.setList(List);
+        return pageResp;
+    }
+
+
+    public void delete(Long id){
+        ${domain}Mapper.deleteByPrimaryKey(id);
+    }
+
+}
--- generator/src/main/java/com/jiawa/train/generator/server/ServerGenerator.java
+++ generator/src/main/java/com/jiawa/train/generator/server/ServerGenerator.java
@@ -1,5 +1,6 @@
 package com.jiawa.train.generator.server;
 
+import com.jiawa.train.generator.util.FreemarkerUtil;
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.Node;
@@ -10,36 +11,58 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ServerGenerator {
-    static String toPath = "generator/src/main/java/com/jiawa/train/generator/test/";
+    static String servicePath = "[module]/src/main/java/com/jiawa/train/[module]/service/";
     static String pomPath ="generator/pom.xml";
     static {
-      new  File(toPath).mkdirs();
+      new  File(servicePath).mkdirs();
     }
     public static void main(String[] args) throws Exception {
+        /*获取mybatis-generator*/
         String generatorPath = getGeneratorPath();
+        /*比如generator-config-member.xml，得到module = member*/
+        String module = generatorPath.replace("src/main/resources/generator-config-","").replace(".xml","");
+        System.out.println("module: "+module);
 
-        Document document = new SAXReader().read("generator/" + generatorPath);
-        Node table = document.selectSingleNode("//table");
+        servicePath = servicePath.replace("[module]",module);//将[module]替换成真正的模块名字
+        System.out.println("servicePath " + servicePath);
+
+        //读取table节点 取generator-config-xxx.xml文件中的table属性
+        Document document = new SAXReader().read("generator/" + generatorPath);/*generatorPath是src/main/resources/generator-config-，前面填上gen
+                                                                                        erator模块名 加起来就是generator/src/main/resources/generator-config-*/
+        Node table = document.selectSingleNode("//table");//取generator-config-xxx.xml文件中的table属性
         System.out.println(table);
-        Node tableName = table.selectSingleNode("@tableName");
+        Node tableName = table.selectSingleNode("@tableName");//前面加上@就是查询属性 读取tableName，domainObjectName这两个属性
         Node domainObjectName = table.selectSingleNode("@domainObjectName");
         System.out.println(tableName.getText() + "/" + domainObjectName.getText());
 
+/*更改ftl模板文件中的￥{domain}变量*/
+        // 示例：表名 jiawa_test
+        // Domain = JiawaTest
+        String Domain = domainObjectName.getText();//获取domain中的单词 开头是大写的
+        // domain = jiawaTest
+        String domain = Domain.substring(0, 1).toLowerCase() + Domain.substring(1);//将大写开头换为小写
+        // do_main = jiawa-test
+        String do_main = tableName.getText().replaceAll("_", "-");
 
+        // 组装参数
+        Map<String, Object> param = new HashMap<>();
+        param.put("Domain", Domain);
+        param.put("domain", domain);
+        param.put("do_main", do_main);
+        System.out.println("组装参数：" + param);
 
-//        FreemarkerUtil.initConfig("test.ftl");
-//        Map<String, Object> param = new HashMap<>();
-//        param.put("domain","Test");
-//        FreemarkerUtil.generator(toPath + "Test.java",param);
+        FreemarkerUtil.initConfig("service.ftl");//生成代码要使用的模板名字（需要更改）
+
+        FreemarkerUtil.generator(servicePath + Domain + "Service.java",param);//生成代码要使用的路径，和生成文件的类名
 
     }
-
+    /*读一个xml文件*/
     private static String getGeneratorPath() throws DocumentException {
         SAXReader saxReader = new SAXReader();
         Map<String, String> map = new HashMap<String, String>();
         map.put("pom", "http://maven.apache.org/POM/4.0.0");
         saxReader.getDocumentFactory().setXPathNamespaceURIs(map);
-        Document document = saxReader.read(pomPath);
+        Document document = saxReader.read(pomPath);//读上面定义好的pom.xml文件中的内容 指向generator-config-xxx.xml
         Node node = document.selectSingleNode("//pom:configurationFile");
         System.out.println(node.getText());
         return node.getText();
