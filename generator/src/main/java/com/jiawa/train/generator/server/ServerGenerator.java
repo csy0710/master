f96984d339299b8dc5198711689bced3eb4cdce3
csy0710 <csy0710@qq.com>
2025-09-09 18:11:47 +0800

    制作queryResp.ftl模板

--- generator/src/main/java/com/jiawa/train/generator/server/ServerGenerator.java
+++ generator/src/main/java/com/jiawa/train/generator/server/ServerGenerator.java
@@ -83,11 +83,11 @@ public class ServerGenerator {
         param.put("typeSet", typeSet);
         System.out.println("组装参数：" + param);
 
-        gen(Domain, param,"service","service");
-        gen(Domain, param,"controller","controller");
-          gen(Domain, param,"req","saveReq");
-        gen(Domain, param, "req", "queryReq");
-
+//        gen(Domain, param,"service","service");
+//        gen(Domain, param,"controller","controller");
+//        gen(Domain, param,"req","saveReq");
+//        gen(Domain, param, "req", "queryReq");
+        gen(Domain, param, "resp", "queryResp");
     }
     /*将输出写成一个方法*/
     private static void gen(String Domain, Map<String, Object> param,String packageName,String target) throws IOException, TemplateException {
