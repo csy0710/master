8e323427eeae5605b05745d07a82b9017964a2dc
csy0710 <csy0710@qq.com>
2025-09-12 17:25:36 +0800

    禁止任务并发执行

--- batch/src/main/java/com/jiawa/train/batch/job/TestJob.java
+++ batch/src/main/java/com/jiawa/train/batch/job/TestJob.java
@@ -1,13 +1,20 @@
 package com.jiawa.train.batch.job;
 
+import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
-
+@DisallowConcurrentExecution  //禁止并发执行
 public class TestJob implements Job {
 
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-        System.out.println("TestJob TEST");
+        System.out.println("TestJob 开始");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("TestJob 结束");
     }
 }
