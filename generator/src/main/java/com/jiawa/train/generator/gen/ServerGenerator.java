2f399cdd26c85b9b5d7a818551189e14d992303f
csy0710 <csy0710@qq.com>
2025-09-12 22:56:01 +0800

    增加每日座位表，每日座位页面改成只读

--- generator/src/main/java/com/jiawa/train/generator/gen/ServerGenerator.java
+++ generator/src/main/java/com/jiawa/train/generator/gen/ServerGenerator.java
@@ -19,7 +19,7 @@ import java.util.Set;
 import static com.jiawa.train.generator.util.DbUtil.getJavaTypes;
 
 public class ServerGenerator {
-    static boolean readOnly = false;/*权限管理，如果不是会员，部分功能不会对其开放。*/
+    static boolean readOnly = true;/*权限管理，如果不是会员，部分功能不会对其开放。*/
     static String vuePath = "admin/src/views/main/";
     static String serverPath = "[module]/src/main/java/com/jiawa/train/[module]/";
     static String pomPath ="generator/pom.xml";
