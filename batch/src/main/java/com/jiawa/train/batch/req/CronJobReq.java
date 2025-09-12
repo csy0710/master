938f3a02075ae2578703adac6b949fd2933bcc3f
csy0710 <csy0710@qq.com>
2025-09-12 17:47:47 +0800

    增加qurtz相关的十二张表，增加接口来操作qurtz

---
+++ batch/src/main/java/com/jiawa/train/batch/req/CronJobReq.java
@@ -0,0 +1,54 @@
+package com.jiawa.train.batch.req;
+
+public class CronJobReq {
+    private String group;    //quartz对任务分组
+
+    private String name;      //每个任务起一个名字 关联任务和quartz
+
+    private String description;  //任务的描述
+
+    private String cronExpression;   //cron表达式
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("CronJobDto{");
+        sb.append("cronExpression='").append(cronExpression).append('\'');
+        sb.append(", group='").append(group).append('\'');
+        sb.append(", name='").append(name).append('\'');
+        sb.append(", description='").append(description).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public String getCronExpression() {
+        return cronExpression;
+    }
+
+    public void setCronExpression(String cronExpression) {
+        this.cronExpression = cronExpression;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
