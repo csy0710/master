a9c7d3e2f63aa487f268f01b444bb3d9d69a58e2
csy0710 <csy0710@qq.com>
2025-09-12 16:10:37 +0800

    增加batch定时调度模块

---
+++ batch/src/main/java/com/jiawa/train/batch/config/BatchApplication.java
@@ -0,0 +1,24 @@
+package com.jiawa.train.batch.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+
+@SpringBootApplication
+@ComponentScan("com.jiawa")
+@MapperScan("com.jiawa.train.*.mapper")
+public class BatchApplication {
+
+    private static final Logger L0G = LoggerFactory.getLogger(BatchApplication.class);
+    //启动日志
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(BatchApplication.class);
+        Environment env = app.run(args).getEnvironment();//拿到整个项目运行的环境
+        L0G.info("启动成功!!");
+        L0G.info("测试地址:\thttp://127.0.0.1:{}{}/hello", env.getProperty("server.port"),env.getProperty("server.servlet.context-path"));//通过环境获取系统的变量 server.port是启动端口
+    }
+}
