2c5910606cc344707500d007a264bdb0e22008b8
csy0710 <csy0710@qq.com>
2025-09-17 19:23:11 +0800

    会员端增加余票查询页面，功能和后台端相同

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -23,6 +23,9 @@ const routes = [
       {
         path: 'passenger',
         component: () => import('../views/main/passenger.vue'),
+      },{
+        path: 'ticket',
+        component: () => import('../views/main/ticket.vue'),
       },]
   },{
     path: '',//访问根域名（‘/’）直接跳转到根域名下welcome，/main/welcome
