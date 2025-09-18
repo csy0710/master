e9258f285db4648afb22f51ec0d4db731a7c5ce3
csy0710 <csy0710@qq.com>
2025-09-18 23:21:26 +0800

    确认提交时，计算出每个乘客所选的座位

--- web/src/views/main/order.vue
+++ web/src/views/main/order.vue
@@ -56,7 +56,8 @@
   </div>
   <a-modal v-model:visible="visible" title="请核对以下信息"
            style="top: 50px; width: 800px"
-           ok-text="确认" cancel-text="取消">
+           ok-text="确认" cancel-text="取消"
+           @ok="handleOk">
     <div class="order-tickets">
       <a-row class="order-tickets-header" v-if="tickets.length > 0">
         <a-col :span="3">乘客</a-col>
@@ -97,6 +98,8 @@
         </div>
         <div style="color: #999999">提示：您可以选择{{tickets.length}}个座位</div>
       </div>
+      <br/>
+      最终购票：{{tickets}}
       </div>
   </a-modal>
 </template>
@@ -294,11 +297,53 @@ export default defineComponent({
           }
         }
       }
-
-
-
       visible.value = true;
     };
+
+    const handleOk = () => {
+      console.log("选好的座位：", chooseSeatObj.value);
+
+      // 设置每张票的座位
+      // 先清空购票列表的座位，有可能之前选了并设置座位了，但选座数不对被拦截了，又重新选一遍
+      for (let i = 0; i < tickets.value.length; i++) {
+        tickets.value[i].seat = null;
+      }
+      let i = -1;
+      // 要么不选座位，要么所选座位应该等于购票数，即i === (tickets.value.length - 1)
+      for (let key in chooseSeatObj.value) {
+        if (chooseSeatObj.value[key]) {
+          i++;
+          if (i > tickets.value.length - 1) {
+            notification.error({description: '所选座位数大于购票数'});
+            return;
+          }
+          tickets.value[i].seat = key;
+        }
+      }
+      if (i > -1 && i < (tickets.value.length - 1)) {
+        notification.error({description: '所选座位数小于购票数'});
+        return;
+      }
+
+      console.log("最终购票：", tickets.value);
+
+      axios.post("/business/confirm-order/do", {
+        dailyTrainTicketId: dailyTrainTicket.id,
+        date: dailyTrainTicket.date,
+        trainCode: dailyTrainTicket.trainCode,
+        start: dailyTrainTicket.start,
+        end: dailyTrainTicket.end,
+        tickets: tickets.value
+      }).then((response) => {
+        let data = response.data;
+        if (data.success) {
+          notification.success({description: "下单成功！"});
+        } else {
+          notification.error({description: data.message});
+        }
+      });
+    }
+
 
     onMounted(()=>{
       handleQueryPassenger();
@@ -315,7 +360,8 @@ export default defineComponent({
       finishCheckPassenger,
       chooseSeatObj,
       chooseSeatType,
-      SEAT_COL_ARRAY
+      SEAT_COL_ARRAY,
+      handleOk
     }
   }
 })
