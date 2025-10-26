169b594123bb95f5aa150afabdaa277f21119d36
csy0710 <csy0710@qq.com>
2025-10-26 22:27:53 +0800

    增加乘客车票表，生成前后端代码

---
+++ member/src/main/java/com/jiawa/train/member/service/TicketService.java
@@ -0,0 +1,72 @@
+package com.jiawa.train.member.service;
+
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.date.DateTime;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.jiawa.train.common.req.MemberTicketReq;
+import com.jiawa.train.common.resp.PageResp;
+import com.jiawa.train.common.util.SnowUtil;
+import com.jiawa.train.member.domain.Ticket;
+import com.jiawa.train.member.domain.TicketExample;
+import com.jiawa.train.member.mapper.TicketMapper;
+import com.jiawa.train.member.req.TicketQueryReq;
+import com.jiawa.train.member.resp.TicketQueryResp;
+import jakarta.annotation.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TicketService {
+      public static final Logger LOG = LoggerFactory.getLogger(TicketService.class);
+    @Resource
+    private TicketMapper ticketMapper;
+    public void save(MemberTicketReq req) throws Exception{
+        DateTime now = DateTime.now();
+        // 将请求对象req的属性复制到Ticket对象中（需要确保两个类的属性名和类型匹配）
+        Ticket ticket = BeanUtil.copyProperties(req, Ticket.class);
+
+            /*新增保存*/
+            ticket.setId(SnowUtil.getSnowflakeNextId());
+            ticket.setCreateTime(now);
+            ticket.setUpdateTime(now);
+            ticketMapper.insert(ticket);
+
+
+    }
+
+    public PageResp<TicketQueryResp> queryList(TicketQueryReq req){
+        TicketExample ticketExample = new TicketExample();// 创建MyBatis的Example查询对象
+        ticketExample.setOrderByClause("id desc");
+        TicketExample.Criteria criteria = ticketExample.createCriteria();    // 创建查询条件Criteria对象
+
+        /*在TicketQueryReq req传入的参数中包含页码和页数*/
+        LOG.info("查询页码：{}",req.getPage());
+        LOG.info("每条页数：{}",req.getSize());
+        /*分页组件*/
+        PageHelper.startPage(req.getPage(), req.getSize());
+        /*执行分页查询，得到的是“当前页”的数据列表，List大小 <= req.getSize()*/
+        List<Ticket> ticketList = ticketMapper.selectByExample(ticketExample);
+        // 将实体列表转换为响应对象列表（DTO转换）
+        List<TicketQueryResp> List = BeanUtil.copyToList(ticketList, TicketQueryResp.class);
+        /*获取包含“总记录数”和“当前页数据”的分页信息对象*/
+        PageInfo<Ticket> pageInfo = new PageInfo<>(ticketList);
+        LOG.info("总行数：{}",pageInfo.getTotal());
+        LOG.info("总页数：{}",pageInfo.getPages());
+       /* 创建自定义的响应对象 PageResp*/
+        PageResp<TicketQueryResp> pageResp = new PageResp<>();
+        /*将“总记录数”和“当前页的DTO列表”设置到自定义响应对象中*/
+        pageResp.setTotal(pageInfo.getTotal());
+        pageResp.setList(List);
+        return pageResp;
+    }
+
+
+    public void delete(Long id){
+        ticketMapper.deleteByPrimaryKey(id);
+    }
+
+}
