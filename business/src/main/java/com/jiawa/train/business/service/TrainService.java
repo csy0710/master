c94c6e2e5fb06cd5c15c8a824097b2c98017a03c
csy0710 <csy0710@qq.com>
2025-10-29 22:40:08 +0800

    mybatis的一级缓存测试

--- business/src/main/java/com/jiawa/train/business/service/TrainService.java
+++ business/src/main/java/com/jiawa/train/business/service/TrainService.java
@@ -20,6 +20,7 @@ import jakarta.annotation.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -90,8 +91,11 @@ public class TrainService {
     }
 
     //查询所有车次编号
+    @Transactional
     public List<TrainQueryResp> queryAll() {
         List<Train> trainList = selectAll();
+//        LOG.info("再查一次");
+//        trainList = selectAll();
         return BeanUtil.copyToList(trainList, TrainQueryResp.class);
     }
 
